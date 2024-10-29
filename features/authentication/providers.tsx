446d687516472e12a16f9053e95d96f3abaa4532
Dorian Cottle <dorian.cottle@students.snow.edu>
2024-10-29 09:58:49 -0600

    oauth works now

--- features/authentication/providers.tsx
+++ features/authentication/providers.tsx
@@ -5,6 +5,7 @@ import { AuthProvider, AuthProviderProps } from "react-oidc-context";
 
 
 const oidcConfig: AuthProviderProps = {
+  // authority: "https://dev-zrzezw0kftjiq5sl.us.auth0.com/",
   authority: "https://auth.snowse.duckdns.org/realms/advanced-frontend/",
   client_id: "dorian-class-demo",
   redirect_uri: "http://localhost:3000/",
