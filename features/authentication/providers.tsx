69eb2297e3a34a839775b4a2d0b7fdca70876a6e
Dorian C <thenewdorian21@gmail.com>
2024-11-04 10:56:54 -0700

    envs

--- features/authentication/providers.tsx
+++ features/authentication/providers.tsx
@@ -2,13 +2,16 @@
 import {ReactNode} from "react"
 import { AuthProvider, AuthProviderProps } from "react-oidc-context";
 
-
+const uri = process.env.NEXT_PUBLIC_REDIRECT_URI;
+ 
+if (!uri) console.log("REDIRECT_URI is not set");
+else console.log("REDIRECT_URI is: " +  uri );
 
 const oidcConfig: AuthProviderProps = {
   // authority: "https://dev-zrzezw0kftjiq5sl.us.auth0.com/",
   authority: "https://auth.snowse.duckdns.org/realms/advanced-frontend/",
   client_id: "dorian-class-demo",
-  redirect_uri: "http://localhost:3000/",
+  redirect_uri: `${uri}`,
 
   onSigninCallback: async (user) => {
     const newUrl = window.location.href.split('?')[0];
